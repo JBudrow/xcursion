236bf28a1cd1998d3bcc04eb164f9eefc612ab32
jbudrow <jbudrowd3@yahoo.com>
2016-04-24 14:25:35 -0400

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  validates_presence_of :password, :email
+  validates :password, length: { minimum: 4 }
+  validates :email, format: { with: /.+@.+/, message: 'Invalid email address' }
+
   has_many :trails
   has_many :trails, through: :provinces
 end
